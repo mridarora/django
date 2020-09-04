60b06bcdd5eac12bb3f219d6f7efa376584e7579
mridarora <aroram15@mcmaster.ca>
2020-09-05 01:00:17 +0530

    random stuff

--- project/project/urls.py
+++ project/project/urls.py
@@ -20,6 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', include("hello.urls")),
     path('newyear/', include("newyear.urls")),
-    path('main/', include("main.urls"))
+    path('main/', include("main.urls")),
     path('tasks/', include("tasks.urls"))
 ]
